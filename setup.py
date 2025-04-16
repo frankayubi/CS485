c36c3f380bdc313c5e7ec8f5ceba46e4e244985a
fay3 <98561343+frankayubi@users.noreply.github.com>
2025-04-16 18:19:01 -0400

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Frank Yubi',
+    author_email= 'frankayubi@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
